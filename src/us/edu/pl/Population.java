58e58f99c87a12cf8ecb0026be9a28bd471b93ba
Bartłomiej Szczotka <szczotka.bartlomiej@gmail.com>
2021-01-31 13:46:25 +0100

    - todo - print total length sequance and fitness

--- src/us/edu/pl/Population.java
+++ src/us/edu/pl/Population.java
@@ -10,8 +10,16 @@ public class Population {
         for(Individual individual : lfsr){
             totalSeqLength += individual.max_seq;
         }
+        System.out.println("\nTotalSeqLength: " + totalSeqLength);
         for (Individual individual : lfsr){
             individual.fitness=(float)individual.max_seq/this.totalSeqLength;
+
+            System.out.println("\nFintess: " + individual.fitness);
         }
+
+        //find 5 best fintess score (the highest number)
+        //for each best fitness I need to find corresponding chromosome (on which we do calculatin of this fitness)
+
+
     }
 }
